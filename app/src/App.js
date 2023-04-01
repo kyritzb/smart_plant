358e12963a2c1dda794c390f5131c1eab7cdb658
Bryan Kyritz <bryan@securemeeting.org>
2023-04-01 17:24:48 -0400

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [humidity, setHumidity] = useState(null);
+
+  useEffect(() => {
+    // Fetch humidity data from server
+    axios
+      .post("/humidity")
+      .then((response) => setHumidity(response.data.humidity))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Humidity</h1>
+        {humidity !== null ? <p>{humidity}%</p> : <p>Loading...</p>}
       </header>
     </div>
   );
