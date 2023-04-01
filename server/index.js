8878e42a0f2351c6242f2538299c466217c38535
Bryan Kyritz <bryan@securemeeting.org>
2023-04-01 18:22:48 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const i2c = require("i2c-bus");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const address = 8; // Address of the Arduino
 const command = 0x01; // Command to read humidity value
@@ -17,12 +17,16 @@ app.use(bodyParser.json());
 app.post("/humidity", (req, res) => {
   // Send command to Arduino and read response
   const buffer = Buffer.alloc(1);
-  bus.readI2cBlockSync(address, command, 1, buffer);
-  const humidity = buffer.readUInt8(0);
 
-  console.log(`Humidity: ${humidity}%`);
-
-  res.json({ humidity });
+  bus.readI2cBlock(address, command, 1, buffer, (err, bytesRead, buffer) => {
+    if (err) {
+      console.error(`Failed to read humidity: ${err}`);
+      return;
+    }
+    const humidity = buffer.readUInt8(0);
+    console.log(`Humidity: ${humidity}%`);
+    res.json({ humidity });
+  });
 });
 
 setInterval(() => {
